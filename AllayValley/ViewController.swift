01d14c2c00ddf862bdcb86d288b3e4fb9055e1a1
Cindy <cindyczy061204@gmail.com>
2022-08-16 14:21:06 -0400

    Initial Commit

---
+++ AllayValley/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AllayValley
+//
+//  Created by Zhiyi Chen on 8/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
